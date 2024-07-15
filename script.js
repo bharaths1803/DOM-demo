6125e5695283348b42c2e117901c2e2be849392a
bharaths1803 <bharaths14051803@gmail.com>
2024-07-15 20:19:35 +0530

    Saving DOM demo details

---
+++ script.js
@@ -0,0 +1,12 @@
+function foo() {
+  var res = document.getElementById("first").value;
+  console.log(res);
+  res = document.getElementById("mid").value;
+  console.log(res);
+  res = document.getElementById("last").value;
+  console.log(res);
+  res = document.getElementById("mail").value;
+  console.log(res);
+  res = document.getElementById("pass").value;
+  console.log(res);
+}
